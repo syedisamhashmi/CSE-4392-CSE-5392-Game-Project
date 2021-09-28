dd4f4cab1c9fe5a8f47aa7f14f2f536d5c6049b1
Ediful <56943939+Ediful@users.noreply.github.com>
2021-09-28 17:52:57 -0500

    added new movement system for banana

--- Team_Hashmi_Project/entities/player/player.gd
+++ Team_Hashmi_Project/entities/player/player.gd
@@ -1,35 +1,51 @@
 extends KinematicBody2D
 
-
+# some of these variables could be exported for easy modification
+# in the editor's user interface
+var topSpeed: float = 1000.0
+var acceleration: float = 0.0
+var friction: float = 0.05
 var velocity: Vector2 = Vector2.ZERO
 var gravity: float = 900.0
-var move_speed: Vector2 = Vector2(300.0, 500.0)
 
 func _physics_process(delta: float) -> void:
-    var isJumpInterrupted = Input.is_action_just_released("jump") and velocity.y < 0
-    var direction: Vector2 = Vector2(
-        Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-        -1 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
-    )
-    velocity.x += (move_speed.x * direction.x) * delta
-    if velocity.x > move_speed.x:
-        velocity.x = move_speed.x
+    # control player through acceleration to give good sense of momentum
+    if Input.is_action_pressed("move_right"):
+      acceleration = 2500
+    elif Input.is_action_pressed("move_left"):
+      acceleration = -2500
+    else:
+      acceleration = 0
+    
+    # used to allow shorter jumps if jump button is released quickly
+    var isJumpInterrupted : = Input.is_action_just_released("jump") and velocity.y < 0.0
+    
+    # want to feel instan and responsivet, so don't bother with acceleration
+    if Input.is_action_just_pressed("jump") and is_on_floor():
+      velocity.y = -400
+      
+    # checks if jump is interrupted
+    if isJumpInterrupted :
+      velocity.y = 0.0
+    
+    # increases velocity on the x axis every frame
+    # friction slows the player down over time
+    velocity.x += acceleration * delta
+    velocity.x *= (1 - friction)
     
     velocity.y += gravity * delta
-    if direction.y == -1:
-        velocity.y = move_speed.y * direction.y
-        
-    if isJumpInterrupted:
-        velocity.y = 0
-    if (
-        (!Input.is_action_pressed("move_left") and
-        !Input.is_action_pressed("move_right")) or
-        Input.is_action_just_released("move_left") or 
-        Input.is_action_just_released("move_right")):
-        velocity.x /= 1.1   
+    
+    # clamp velocity
+    velocity.x = clamp(velocity.x, -1 * topSpeed, topSpeed)
+    
+    # removes jitter when player is slowing down
+    if abs(velocity.x) < 10:
+      velocity.x = 0
+    
+    # Godot's built in function to determine final velocity
     velocity = move_and_slide(velocity, Vector2.UP)
-
-
+    
+    
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     pass # Replace with function body.
