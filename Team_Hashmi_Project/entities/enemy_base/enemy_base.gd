0c5f759c5710ad4fe0d6e3579f18777deb7d121a
Syed Isam Hashmi <prosdkr@me.com>
2021-09-29 20:15:01 -0500

    feat: added enemy_base, added layer for projectiles and enemies

---
+++ Team_Hashmi_Project/entities/enemy_base/enemy_base.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+var velocity: Vector2 = Vector2.ZERO
+var gravity: float = 900.0
+
+func _physics_process(delta: float) -> void:
+    velocity.y += gravity * delta
+    # Godot's built in function to determine final velocity
+    velocity = move_and_slide(velocity, Vector2.UP)
+    
