fe287e5f47b058c66f33ad39662289ed579985a7
Ediful <56943939+Ediful@users.noreply.github.com>
2021-10-02 21:12:18 -0500

    feature: added friction to enemy

--- Team_Hashmi_Project/entities/enemy_base/enemy_base.gd
+++ Team_Hashmi_Project/entities/enemy_base/enemy_base.gd
@@ -2,9 +2,11 @@ extends KinematicBody2D
 
 var velocity: Vector2 = Vector2.ZERO
 var gravity: float = 900.0
+var friction: float = 0.9
 
 func _physics_process(delta: float) -> void:
     velocity.y += gravity * delta
+    velocity.x *= friction;
     # Godot's built in function to determine final velocity
     velocity = move_and_slide(velocity, Vector2.UP)
     
