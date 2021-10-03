329b957b2e599f77850eb71c77d62c2b9d600b48
Ediful <56943939+Ediful@users.noreply.github.com>
2021-10-02 20:34:51 -0500

    feature: added damage flash

--- Team_Hashmi_Project/entities/enemy_base/enemy_base.gd
+++ Team_Hashmi_Project/entities/enemy_base/enemy_base.gd
@@ -9,4 +9,11 @@ func _physics_process(delta: float) -> void:
     velocity = move_and_slide(velocity, Vector2.UP)
     
 func damage(knockback):
+  damage_flash_effect()
   velocity.x += knockback
+
+func damage_flash_effect():
+  $Enemy_Base_Image.material.set_shader_param("intensity", 0.75)
+  yield(get_tree().create_timer(0.1), "timeout")
+  $Enemy_Base_Image.material.set_shader_param("intensity", 0.0)
+  
