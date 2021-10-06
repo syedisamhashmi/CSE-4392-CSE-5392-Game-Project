240c817391170135c28410c130af31b86edaf2c3
Syed Isam Hashmi <prosdkr@me.com>
2021-10-05 23:48:10 -0500

    feat: weapon tracking system, unlockable system, save game state for most player attributes

--- Team_Hashmi_Project/Autoloads/Globals.gd
+++ Team_Hashmi_Project/Autoloads/Globals.gd
@@ -1,6 +1,16 @@
 extends Node
 
-var player_stats_file: String = "user://player_stats.tres"
+#region Filenames and Extension constants
+var player_stats_file: String = "user://player_stats_"
+var player_save_file: String = "user://player_save_"
+var TRES_EXTENSION: String = ".tres"
+#endregion
+
+func getPlayerStatsFileName():
+    return player_stats_file + str(PlayerData.getSaveSlot()) + TRES_EXTENSION
+func getPlayerSaveFileName():
+    return player_save_file + str(PlayerData.getSaveSlot()) + TRES_EXTENSION
+
 
 func _ready() -> void:
     # ? Connect the exit_game signal to save stats
@@ -8,53 +18,29 @@ func _ready() -> void:
     # warning-ignore:return_value_discarded
     Signals.connect("exit_game", self, "save_stats")
 
+    # ? Connect the exit_game signal to save the game
+    # ? This way, before the game exits, the game state is saved.
+    # warning-ignore:return_value_discarded
+    Signals.connect("exit_game", self, "save_game")
+
 func load_stats() -> void:
-    var playerStats = loadDataFromFile(player_stats_file, {"shotsFired": 0})
+    var playerStats = Utils.loadDataFromFile(getPlayerStatsFileName(), 
+        PlayerData.getDefaultPlayerStats()
+    )
     PlayerData.setPlayerData(playerStats)
         
 func save_stats() -> void:
-    saveDataToFile(player_stats_file, PlayerData.playerData)
-    
-    
-func loadDataFromFile(fileName: String, default: Dictionary = {}) -> Dictionary:
-    # Check if file exists
-    if doesFileExist(fileName):
-        # Default location is hard to find,
-        # so I printed it out for you, you are welcome.
-        print("Found file " + fileName + " in " + OS.get_user_data_dir())
-        var file = File.new()
-        file.open(fileName, File.READ)
-        print("Reading Data: ")
-        var fileData = file.get_as_text() 
-        print(fileData)
-        file.close()
-        # Return data as a dictionary
-        return parse_json(fileData)
-    else:
-        print("Could not find file \"" + fileName + "\" creating it now.")
-        var file = File.new()
-        # If we can't find the file,
-        # create it with an empty dict by default
-        file.open(fileName, File.WRITE)
-        print("Writing data to \"" + fileName + "\":")
-        print(default)
-        file.store_string(to_json(default))
-        file.close()
-        return default
+    Utils.saveDataToFile(getPlayerStatsFileName(), PlayerData.playerData)
 
-# Just a wrapper function, easier to read.
-func doesFileExist(fileName: String) -> bool:
-    return ResourceLoader.exists(fileName)  
-    
-func saveDataToFile(fileName: String, data: Dictionary) -> void:
-    var file = File.new()
-    # Try to open the file, to create it if it doesn't exist.
-    var _oldData = loadDataFromFile(fileName)
-    # Open the file
-    file.open(fileName, File.WRITE)
-    print("Writing data to \"" + fileName + "\":")
-    # Print what we are about to write
-    print(to_json(data))    
-    # Write the data
-    file.store_string(to_json(data))
-    file.close()
+
+func load_game() -> void:
+    var saveGame = Utils.loadDataFromFile(
+        getPlayerSaveFileName(), 
+        PlayerData.getDefaultSaveGame(
+            PlayerData.DIFFICULTIES.IM_TOO_SQUISHY_TO_DIE
+        )
+    )
+    PlayerData.setSavedGame(saveGame)
+
+func save_game() -> void:
+    Utils.saveDataToFile(getPlayerSaveFileName(), PlayerData.savedGame)
--- Team_Hashmi_Project/Autoloads/PlayerData.gd
+++ Team_Hashmi_Project/Autoloads/PlayerData.gd
@@ -1,10 +1,26 @@
 extends Node
 
-var playerData: Dictionary = {} setget setPlayerData, getPlayerData
+var saveSlot: int = 0 setget setSaveSlot, getSaveSlot
+func getSaveSlot() -> int:
+    return saveSlot
+func setSaveSlot(i: int) -> void:
+    saveSlot = i
 
-
+#region PlayerStats
+# Various keys in our stat tracker
 var SHOTS_FIRED: String = "shotsFired"
+var PUNCHES_THROWN: String = "punchesThrown"
 var JUMP_COUNT: String = "jumpCount"
+var BANANAS_THROWN: String = "bananasThrown"
+# Object containing default values for player statistics
+var DEFAULT_PLAYER_STATS: Dictionary = {
+    PUNCHES_THROWN: 0,
+    BANANAS_THROWN: 0,
+    SHOTS_FIRED: 0,
+    JUMP_COUNT: 0
+}
+func getDefaultPlayerStats() -> Dictionary:
+    return DEFAULT_PLAYER_STATS 
 
 #region jumpCount
 func getJumpCount() -> int:
@@ -15,18 +31,126 @@ func getJumpCount() -> int:
 func setJumpCount(a) -> void:
     playerData[JUMP_COUNT] = a
 #endregion
-
-#region shotsFired
-func getShotsFired() -> int:
-    if !playerData.has(SHOTS_FIRED):
-        setShotsFired(0)
+#region bananasThrown
+func getBananasThrown() -> int:
+    if !playerData.has(BANANAS_THROWN):
+        setBananasThrown(0)
         return 0
-    return playerData[SHOTS_FIRED]
-func setShotsFired(a) -> void:
-    playerData[SHOTS_FIRED] = a
+    return playerData[BANANAS_THROWN]
+func setBananasThrown(a) -> void:
+    playerData[BANANAS_THROWN] = a
+#endregion
+#region punchesThrown
+func getPunchesThrown() -> int:
+    if !playerData.has(PUNCHES_THROWN):
+        setJumpCount(0)
+        return 0
+    return playerData[PUNCHES_THROWN]
+func setPunchesThrown(a) -> void:
+    playerData[PUNCHES_THROWN] = a
 #endregion
 
+var playerData: Dictionary = {} setget setPlayerData, getPlayerData
 func getPlayerData() -> Dictionary:
     return playerData
 func setPlayerData(a) -> void:
     playerData = a
+#endregion
+
+#region SaveData
+
+# TODO: these are going to be labels on the UI,
+# Furthermore, they will probably be translated into numbers
+#   as multipliers of enemy damage/health.
+# If you get this throwback, we can be friends :)
+const DIFFICULTIES = {
+    CAN_I_PLAY_DADDY      = "Can I Play, Daddy?",      # Easy
+    IM_TOO_SQUISHY_TO_DIE = "I'm tot squishy to die!", # Medium
+    BRUISE_ME_PLENTY      = "Bruise Me Plenty",        # Hard
+    I_AM_BANANA_INCARNATE = "I Am Banana Incarnate!",  # Nightmare   
+}
+
+# Various keys that will be in save data
+var IS_MELEE_UNLOCKED:        String = "isMeleeUnlocked"
+var IS_BANANA_THROW_UNLOCKED: String = "isBananaThrowUnlocked"
+var DIFFICULTY:               String = "difficulty"
+var PLAYER_HEALTH:            String = "playerHealth"
+var PLAYER_MOVE_SPEED:        String = "playerMoveSpeed"
+var PLAYER_JUMP_HEIGHT:       String = "playerJumpHeight"
+var CURRENT_WEAPON:           String = "currentWeapon"
+
+# Should be called when creating a new save game
+# Returns the default state with the difficulty
+func getDefaultSaveGame(difficulty: String) -> Dictionary:
+    return {
+        PLAYER_HEALTH           : PlayerDefaults.DEFAULT_PLAYER_HEALTH,
+        CURRENT_WEAPON          : PlayerDefaults.DEFAULT_WEAPON,   # Melee
+        IS_MELEE_UNLOCKED       : PlayerDefaults.IS_MELEE_UNLOCKED,
+        IS_BANANA_THROW_UNLOCKED: PlayerDefaults.IS_BANANA_THROW_UNLOCKED,
+        DIFFICULTY              : difficulty,
+        PLAYER_MOVE_SPEED       : PlayerDefaults.PLAYER_MOVE_SPEED,
+        PLAYER_JUMP_HEIGHT      : PlayerDefaults.PLAYER_JUMP_HEIGHT,
+    }
+# ? Object containing default values for game save data
+
+#region PlayerHealth
+func getPlayerHealth() -> int:
+    if !savedGame.has(PLAYER_HEALTH):
+        setCurrentWeapon(PlayerDefaults.DEFAULT_PLAYER_HEALTH)
+        return PlayerDefaults.DEFAULT_PLAYER_HEALTH
+    return savedGame[PLAYER_HEALTH]
+func setPlayerHealth(a) -> void:
+    savedGame[PLAYER_HEALTH] = a
+#endregion
+#region CurrentWeapon
+func getCurrentWeapon() -> int:
+    if !savedGame.has(CURRENT_WEAPON):
+        setCurrentWeapon(PlayerDefaults.CURRENT_WEAPON)
+        return PlayerDefaults.DEFAULT_WEAPON
+    return savedGame[CURRENT_WEAPON]
+func setCurrentWeapon(a) -> void:
+    savedGame[CURRENT_WEAPON] = a
+#endregion
+#region IsMeleeUnlocked
+func getIsMeleeUnlocked() -> bool:
+    if !savedGame.has(IS_MELEE_UNLOCKED):
+        setIsMeleeUnlocked(PlayerDefaults.IS_MELEE_UNLOCKED)
+        return PlayerDefaults.IS_MELEE_UNLOCKED
+    return savedGame[IS_MELEE_UNLOCKED]
+func setIsMeleeUnlocked(a) -> void:
+    savedGame[IS_MELEE_UNLOCKED] = a
+#endregion
+#region IsBananaThrowUnlocked
+func getIsBananaThrowUnlocked() -> bool:
+    if !savedGame.has(IS_BANANA_THROW_UNLOCKED):
+        setIsMeleeUnlocked(PlayerDefaults.IS_BANANA_THROW_UNLOCKED)
+        return PlayerDefaults.IS_BANANA_THROW_UNLOCKED
+    return savedGame[IS_BANANA_THROW_UNLOCKED]
+func setIsBananaThrowUnlocked(a) -> void:
+    savedGame[IS_BANANA_THROW_UNLOCKED] = a
+#endregion
+#region PlayerMoveSpeed
+func getPlayerMoveSpeed() -> float:
+    if !savedGame.has(PLAYER_MOVE_SPEED):
+        setIsMeleeUnlocked(PlayerDefaults.PLAYER_MOVE_SPEED)
+        return PlayerDefaults.PLAYER_MOVE_SPEED
+    return savedGame[PLAYER_MOVE_SPEED]
+func setPlayerMoveSpeed(a) -> void:
+    savedGame[IS_BANANA_THROW_UNLOCKED] = a
+#endregion
+#region PlayerJumpHeight
+func getPlayerJumpHeight() -> float:
+    if !savedGame.has(PLAYER_JUMP_HEIGHT):
+        setIsMeleeUnlocked(PlayerDefaults.PLAYER_JUMP_HEIGHT)
+        return PlayerDefaults.PLAYER_JUMP_HEIGHT
+    return savedGame[PLAYER_JUMP_HEIGHT]
+func setPlayerJumpHeight(a) -> void:
+    savedGame[PLAYER_JUMP_HEIGHT] = a
+#endregion
+
+var savedGame: Dictionary = {} setget setSavedGame, getSavedGame
+func getSavedGame() -> Dictionary:
+    return savedGame
+func setSavedGame(a) -> void:
+    savedGame = a
+#endregion
