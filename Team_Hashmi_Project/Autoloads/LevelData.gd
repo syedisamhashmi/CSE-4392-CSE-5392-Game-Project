1c5ee2fe916466a56967cb732e2fe8deaea77edd
Syed Isam Hashmi <prosdkr@me.com>
2021-10-24 14:15:56 -0500

    feat: added player location to level information, next doing pickups

--- Team_Hashmi_Project/Autoloads/LevelData.gd
+++ Team_Hashmi_Project/Autoloads/LevelData.gd
@@ -1,7 +1,16 @@
 extends Node
 
+#region Player 
 # warning-ignore:unused_class_variable
-var tiles = []
+var playerStartX: float = 0
+# warning-ignore:unused_class_variable
+var playerStartY: float = 0
+#endregion
+
+#region Map
+# warning-ignore:unused_class_variable
+var tiles:              = []
+#endregion
 
 
 func init():
