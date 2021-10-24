875b1c470476e4646b3d4f3999bb7d9c1be09581
Syed Isam Hashmi <prosdkr@me.com>
2021-10-24 14:15:56 -0500

    feat: added player location to level information, next doing pickups

--- Team_Hashmi_Project/entities/Level.gd
+++ Team_Hashmi_Project/entities/Level.gd
@@ -40,7 +40,33 @@ func readMapData():
                 # Which Tile in the tileset to use
                 Vector2(tile.tileCoordX, tile.tileCoordY)
             )
+    #Set player start from saved level information
+    $Banana.position = Vector2(levelData.playerStartX, levelData.playerStartY)
 
+
+func writeMapData():
+    var tm = $World
+    var tileInfo = []    
+    for position in tm.get_used_cells():
+        var tile = getNewTile()
+        print(position)
+        tile.posX = position.x
+        tile.posY = position.y
+        tile.index = tm.get_cell(position.x,position.y)
+        tile.tileCoordX = tm.get_cell_autotile_coord(position.x, position.y).x
+        tile.tileCoordY = tm.get_cell_autotile_coord(position.x, position.y).y
+        tileInfo.append(tile)
+    LevelData.tiles = tileInfo
+    
+    LevelData.playerStartX = $Banana.position.x
+    LevelData.playerStartY = $Banana.position.y
+    
+    Utils.saveDataToFile(
+        "user://level" + str(PlayerData.savedGame.levelNum) + ".dat",
+        LevelData, 
+        true, 
+        false
+    )
 
 func getNewTile():
     return {
@@ -68,6 +94,9 @@ var xt = [2,9,3,14,12,6,21,16,4,15,17,24,7,11,25,19,13,10,20,18,8,5,0,22,1,23]
 var kc = PoolByteArray([])
 # Don't debug for the answer.
 func _input(event: InputEvent) -> void:
+    if Input.is_action_just_released("ui_cancel"):
+        writeMapData()
+    
     # You will be a horrible person.
     if !event is InputEventKey: return
     # I will never forgive you.
