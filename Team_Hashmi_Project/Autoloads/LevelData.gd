b4a2af43a27a95249e85c7357d3b4e2e78461360
Syed Isam Hashmi <prosdkr@me.com>
2021-10-28 21:44:20 -0500

    feat: removed lvl obj completely, fixed jump

--- Team_Hashmi_Project/Autoloads/LevelData.gd
+++ Team_Hashmi_Project/Autoloads/LevelData.gd
@@ -9,6 +9,35 @@ var playerStartY: float = 0
 
 #region Map
 # warning-ignore:unused_class_variable
+var backgroundPath: String = ""
+# warning-ignore:unused_class_variable
+var backgroundMotionScaleX: float = 0
+# warning-ignore:unused_class_variable
+var backgroundMotionScaleY: float = 0
+# warning-ignore:unused_class_variable
+var backgroundPosX: float = 0
+# warning-ignore:unused_class_variable
+var backgroundPosY: float = 0
+# warning-ignore:unused_class_variable
+var backgroundSizeX: float = 0
+# warning-ignore:unused_class_variable
+var backgroundSizeY: float = 0
+
+# warning-ignore:unused_class_variable
+var layer2MotionScaleX: float = 0
+# warning-ignore:unused_class_variable
+var layer2MotionScaleY: float = 0
+# warning-ignore:unused_class_variable
+var layer2: = []
+# warning-ignore:unused_class_variable
+var layer3MotionScaleX: float = 0
+# warning-ignore:unused_class_variable
+var layer3MotionScaleY: float = 0
+# warning-ignore:unused_class_variable
+var layer3: = []
+
+
+# warning-ignore:unused_class_variable
 var tiles:              = []
 # warning-ignore:unused_class_variable
 var pickups:            = []
