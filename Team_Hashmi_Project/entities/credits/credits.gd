388562eaddfae833791748e88fc10de8c8567b26
Ediful <56943939+Ediful@users.noreply.github.com>
2021-11-12 18:23:18 -0600

    variables for weapon widths and some credits

--- Team_Hashmi_Project/entities/credits/credits.gd
+++ Team_Hashmi_Project/entities/credits/credits.gd
@@ -98,6 +98,15 @@ var credits = [
         {"txt": "", "type": "t"},
         {"txt": "Edward Kressler", "type": "t"},
         #TODO: Edward
+        {"txt":"Animation", "type": "c"},
+        {"txt":"        - Banana-man", "type": "s"},
+        {"txt":"        - Pineapple", "type": "s"},
+        
+        {"txt":"Art", "type": "c"},
+        {"txt":"        - Level 1 Background", "type": "s"},
+        {"txt":"        - Level 1 Tilemap", "type": "s"},
+        {"txt":"        - BFG", "type": "s"},
+        {"txt":"        - Banana Blaster", "type": "s"}
     ],[
         {"txt": "", "type": "t"},
         {"txt": "", "type": "t"},
