fd0d2c5386619247fc459018031b64acbc8d5152
Syed Isam Hashmi <prosdkr@me.com>
2021-11-15 23:32:07 -0600

    fix: put enemy tests back into unit tests, accidentally made them integration

--- Team_Hashmi_Project/test/unit/test_enemy.gd
+++ Team_Hashmi_Project/test/unit/test_enemy.gd
@@ -39,3 +39,42 @@ func test_assert_enemies_proper_types():
     raddish.queue_free()
     spikes.queue_free()
     yield(get_tree().create_timer(1), "timeout")
+
+
+func test_assert_onion_take_damage():
+    var onion = ENEMY_BIG_ONION.instance()
+    add_child(onion)
+    assert_enemy_take_damage(onion, "Onion")
+    yield(get_tree().create_timer(.7), "timeout")
+    onion.queue_free()
+    
+func test_assert_pineapple_take_damage():
+    var pineapple = ENEMY_PINEAPPLE.instance()
+    add_child(pineapple)
+    assert_enemy_take_damage(pineapple, "Pineapple")
+    yield(get_tree().create_timer(.7), "timeout")
+    pineapple.queue_free()
+    
+func test_assert_raddish_take_damage():
+    var raddish = ENEMY_RADDISH.instance()
+    add_child(raddish)
+    assert_enemy_take_damage(raddish, "Raddish")
+    yield(get_tree().create_timer(.7), "timeout")
+    raddish.queue_free()
+    
+func test_assert_spike_take_damage():
+    var spike = ENEMY_SPIKE.instance()
+    add_child(spike)
+    var beforeHealth = spike.health
+    spike.damage(10, 1)
+    var afterHealth = spike.health
+    assert_eq(beforeHealth, afterHealth, "Spikes should NOT have taken damage")
+    yield(get_tree().create_timer(.7), "timeout")
+    spike.queue_free()
+
+func assert_enemy_take_damage(enemy, printName):
+    var beforeHealth = enemy.health
+    enemy.damage(10, 1)
+    var afterHealth = enemy.health
+    assert_true(afterHealth < beforeHealth, printName +" should have taken damage")
+    
