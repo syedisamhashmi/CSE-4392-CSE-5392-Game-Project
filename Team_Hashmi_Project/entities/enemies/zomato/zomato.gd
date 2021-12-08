cbf9a622ce57d45170b445c02766fba6ad1de705
Natraj <hulinatraj@gmail.com>
2021-12-07 19:26:23 -0600

    fixing tomato case sensitive issues

---
+++ Team_Hashmi_Project/entities/enemies/zomato/zomato.gd
@@ -0,0 +1,15 @@
+extends "res://entities/enemies/enemy_base/enemy_base.gd"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  type = EntityTypeEnums.ENEMY_TYPE.TOMATO# Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#  pass
