379843771da1b46892f41ecacf34d16a9e684d99
Natraj <hulinatraj@gmail.com>
2021-12-09 21:43:41 -0600

    edited world

--- Team_Hashmi_Project/entities/enemies/npc/npc.gd
+++ Team_Hashmi_Project/entities/enemies/npc/npc.gd
@@ -1,14 +1,7 @@
 extends "res://entities/enemies/enemy_base/enemy_base.gd"
 
-# Declare member variables here. Examples:
-# var a = 2
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-  type = EntityTypeEnums.ENEMY_TYPE.NPC# Replace with function body.
+  type = EntityTypeEnums.ENEMY_TYPE.NPC
+  $Image.play("idle")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
