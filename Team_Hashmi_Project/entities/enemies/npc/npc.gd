bc15dbcc23812eb4dabf1d73b19c2ce810648e72
Natraj <hulinatraj@gmail.com>
2021-12-09 19:41:15 -0600

    updated the level and enemy type. Collision is disabled.
    Former-commit-id: c915cfed1459f22e643f6ae8b58100f1f1448e60

--- Team_Hashmi_Project/entities/enemies/npc/npc.gd
+++ Team_Hashmi_Project/entities/enemies/npc/npc.gd
@@ -1,14 +1,8 @@
 extends "res://entities/enemies/enemy_base/enemy_base.gd"
 
-# Declare member variables here. Examples:
-# var a = 2
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-  type = EntityTypeEnums.ENEMY_TYPE.NPC# Replace with function body.
+  type = EntityTypeEnums.ENEMY_TYPE.NPC
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
